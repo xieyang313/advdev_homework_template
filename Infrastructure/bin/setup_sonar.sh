635ca2015d0881710829a0c0d96c8e0e47ac4ba4
xieyang <xieyang@csii.com.cn>
2018-09-13 20:48:01 +0800

    修改

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -9,6 +9,8 @@ fi
 GUID=$1
 echo "Setting up Sonarqube in project $GUID-sonarqube"
 
+oc process -f Infrastructure/templates/sonar-template.yaml -n ${GUID}-sonarqube -p GUID=${GUID} | oc create -n ${GUID}-sonarqube -f -
+
 # Code to set up the SonarQube project.
 # Ideally just calls a template
 # oc new-app -f ../templates/sonarqube.yaml --param .....
