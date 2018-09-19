c24bbec39841054ca08b85a307dfaddb13bc429b
xieyang <xieyang@csii.com.cn>
2018-09-19 12:35:27 +0800

    modify setup dev

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -23,59 +23,61 @@ oc policy add-role-to-user edit system:serviceaccount:gpte-jenkins:jenkins -n ${
 oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-parks-dev
 
 #create buildconfig
-oc new-build --binary=true --name="MLBParks" jboss-eap70-openshift:1.7 -n ${GUID}-parks-dev
+oc new-build --binary=true --name="mlparks" jboss-eap70-openshift:1.7 -n ${GUID}-parks-dev
 
-oc new-build --binary=true --name="NationalParks" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
+oc new-build --binary=true --name="nationalparks" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
 
-oc new-build --binary=true --name="ParksMap" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
+oc new-build --binary=true --name="parksmap" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
 
 #create configmap
-oc create configmap MLBParks-config --from-file=./Infrastructure/templates/MLBParks-dev.properties -n ${GUID}-parks-dev
+oc create configmap mlparks-config --from-env-file=./Infrastructure/templates/MLBParks-dev.env -n ${GUID}-parks-dev
 
-oc create configmap NationalParks-config --from-file=./Infrastructure/templates/NationalParks-dev.properties -n ${GUID}-parks-dev
+oc create configmap nationalparks-config --from-env-file=./Infrastructure/templates/NationalParks-dev.env -n ${GUID}-parks-dev
 
-oc create configmap ParksMap-config --from-file=./Infrastructure/templates/ParksMap-dev.properties -n ${GUID}-parks-dev
+oc create configmap parksmap-config --from-env-file=./Infrastructure/templates/ParksMap-dev.env -n ${GUID}-parks-dev
 
 #create app
-oc new-app ${GUID}-parks-dev/MLBParks:0.0-0 --name=MLBParks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+oc new-app ${GUID}-parks-dev/mlparks:0.0-0 --name=mlparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 
-oc new-app ${GUID}-parks-dev/NationalParks:0.0-0 --name=NationalParks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+oc new-app ${GUID}-parks-dev/nationalparks:0.0-0 --name=nationalparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 
-oc new-app ${GUID}-parks-dev/ParksMap:0.0-0 --name=ParksMap --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+oc new-app ${GUID}-parks-dev/parksmap:0.0-0 --name=parksmap --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 
 #remove triggers
-oc set triggers dc/MLBParks --remove-all -n ${GUID}-parks-dev
+oc set triggers dc/mlparks --remove-all -n ${GUID}-parks-dev
 
-oc set triggers dc/NationalParks --remove-all -n ${GUID}-parks-dev
+oc set triggers dc/nationalparks --remove-all -n ${GUID}-parks-dev
 
-oc set triggers dc/ParksMap --remove-all -n ${GUID}-parks-dev
+oc set triggers dc/parksmap --remove-all -n ${GUID}-parks-dev
 
 #set ent for dc
-oc set env dc/MLBParks --from=configmap/MLBParks-config -n ${GUID}-parks-dev
+oc set env dc/mlparks --from=configmap/mlparks-config -n ${GUID}-parks-dev
 
-oc set env dc/NationalParks --from=configmap/NationalParks-config -n ${GUID}-parks-dev
+oc set env dc/nationalparks --from=configmap/nationalparks-config -n ${GUID}-parks-dev
 
-oc set env dc/ParksMap --from=configmap/ParksMap-config -n ${GUID}-parks-dev
+oc set env dc/parksmap --from=configmap/parksmap-config -n ${GUID}-parks-dev
 
-oc set probe dc/ParksMap --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
-oc set probe dc/ParksMap --readiness --failure-threshold 5 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+oc set probe dc/parksmap --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
+oc set probe dc/parksmap --readiness --failure-threshold 5 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
 
-oc set probe dc/MLBParks --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
-oc set probe dc/MLBParks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+oc set probe dc/mlparks --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
+oc set probe dc/mlparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
 
-oc set probe dc/NationalParks --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
-oc set probe dc/NationalParks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+oc set probe dc/nationalparks --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
+oc set probe dc/nationalparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
 
 #expose svcs
-oc expose dc MLBParks --port 8080 -n ${GUID}-parks-dev
+oc expose dc mlparks --port 8080 -n ${GUID}-parks-dev
 
-oc expose dc NationalParks --port 8080 -n ${GUID}-parks-dev
+oc expose dc nationalparks --port 8080 -n ${GUID}-parks-dev
 
-oc expose dc ParksMap --port 8080 -n ${GUID}-parks-dev
+oc expose dc parksmap --port 8080 -n ${GUID}-parks-dev
 
-oc expose svc MLBParks -n ${GUID}-parks-dev --labels="type=parksmap-backend"
+oc expose svc mlparks -n ${GUID}-parks-dev --labels="type=parksmap-backend"
 
-oc expose svc NationalParks -n ${GUID}-parks-dev --labels="type=parksmap-backend"
+oc expose svc nationalparks -n ${GUID}-parks-dev --labels="type=parksmap-backend"
 
-oc expose svc ParksMap -n ${GUID}-parks-dev
+oc expose svc parksmap -n ${GUID}-parks-dev
+
+oc create configmap mlparks-config --from-env-file=test.env -n ${GUID}-parks-dev
 
